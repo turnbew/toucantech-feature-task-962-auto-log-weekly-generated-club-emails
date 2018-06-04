72879e3838b1f5316b8f91470769db8806225081
turnbew <turnbew.co.uk@gmail.com>
2018-06-04 18:21:11 +0100

    Delete separator_section.php

--- addons/default/modules/clubs/views/email_templates/weekly_newsletters/separator_section.php
+++
@@ -1,6 +0,0 @@
-						
-						<tr><td style="height: 8px; padding: 0px !important;"></td></tr>
-						
-						<tr><td style="height: 8px; background-color: #f5f5f5; padding: 0px !important;"></td></tr>
-						
-						<tr><td style="height: 8px; padding: 0px !important;"></td></tr>
